704f412ea3297ca4784b14672b7110666f60ab95
Lindsay Hamoudi <lhamoudi@twilio.com>
2021-01-20 14:44:34 -0800

    Extra param to specify specific user SIDs to focus on

--- scripts/chat/listUsersAndChannels.js
+++ scripts/chat/listUsersAndChannels.js
@@ -1,6 +1,17 @@
 require("dotenv").config();
 const fs = require("fs");
 const Twilio = require("twilio");
+const args = require('yargs')(process.argv.slice(2))
+    .array('userSid')
+    .usage('Usage: $0 --userSid=[USxxx]')
+    .describe('userSid', '(Optional) Specific SID of user to look for')
+    .default('startDate', '4 hours ago')
+    .describe('endDate', 'End of search range')
+    .default('endDate', 'Current time')
+    .describe('includeColumn', 'Extra column(s) to display')
+    .describe('excludeEventType', 'Event(s) to be excluded from results')
+
+    .argv;
 
 const accountSid = process.env.TWILIO_ACCT_SID;
 const authToken = process.env.TWILIO_ACCT_AUTH;
@@ -29,29 +40,42 @@ UserWithChannels.prototype.compareTo = function (other) {
 
 
 
-async function outputUsers(filename) {
+async function outputUsers(userSids, filename) {
     filename = !!filename ? filename : "output.csv";
 
     let usersWithChannels = [];
 
     console.log(`Output file will be: ${filename}`);
 
-    const workerNames = await getWorkers();
-    console.log(`Found ${workerNames.length} total workers. Retrieving corresponding chat users...`);
+    let userIdentities = [];
+
+    if (!!userSids) {
+        // Use the user SID values as user identity
+        userIdentities = userSids;
+    } else {
+        // Get all workers and then derive user identity
+        const workerNames = await getWorkers();
+    
+        for (let i = 0; i < workerNames.length; i++) {
+            // Convert the worker name to the chat user identity (at time of writing, '.' and '@' in email addresses are converted)
+            const escapedWorkerName = escapeNonAlphaChars(workerNames[i]);  
+            userIdentities.push(escapedWorkerName);
+        }     
+
+        console.log(`Found ${userIdentities.length} total workers.`);
+    }
+    console.log(`Retrieving ${userIdentities.length} chat users...`);
 
     // Use for loop to sequentially pull each corresponding chat user
     // (slow, but avoids hammering API and getting 'Too many requests' errors)
-    for (let i = 0; i < workerNames.length; i++) {
-        // Convert the worker name to the chat user identity (at time of writing, '.' and '@' in email addresses are converted)
-        const escapedWorkerName = escapeNonAlphaChars(workerNames[i]);
-
-        const user = await getUser(escapedWorkerName);
+    for (let i = 0; i < userIdentities.length; i++) {
+        const user = await getUser(userIdentities[i]);
         if (user && user.sid) {
             usersWithChannels.push(new UserWithChannels(user.friendlyName, user.sid, user.joinedChannelsCount));
         } else {
-            console.error(`Failed to load user with identity: ${escapedWorkerName}`);
+            console.error(`Failed to load user with identity: ${userIdentities[i]}`);
         }
-        ((i+1) % 10 == 0) && console.log(`Processed ${i+1} of ${workerNames.length}`);
+        ((i+1) % 10 == 0) && console.log(`Processed ${i+1} of ${userIdentities.length}`);
     }
     console.log(`Found ${usersWithChannels.length} users.`);
     const sortedList = usersWithChannels.sort((a, b) => a.compareTo(b));
@@ -114,4 +138,8 @@ function extractAsCSV(usersAndChannels) {
     return header.concat(rows).join("\n");
 
 }
-outputUsers();
+
+
+const userSids = args.userSid;
+
+outputUsers(userSids);
